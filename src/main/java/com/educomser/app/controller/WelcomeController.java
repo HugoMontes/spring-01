6d1bd1e438e25ff724d889f78fd0433ae45551d8
hmontes <hmontes@cofar.com.bo>
2021-05-14 11:41:12 -0400

    Inicio MVC

--- src/main/java/com/educomser/app/controller/WelcomeController.java
+++ src/main/java/com/educomser/app/controller/WelcomeController.java
@@ -1,15 +1,15 @@
 package com.educomser.app.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/welcome")
 public class WelcomeController {
 
 	@GetMapping("/saludo")
 	public String index() {
-		return "Bienvenidos al curso de Spring boot";
+		return "welcome/index";
 	}
 }
